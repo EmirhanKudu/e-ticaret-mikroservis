2fd307c3ecfe9b800035da22945dd4569ff2c42e
Emirhan Kudu <ekudu16@gmail.com>
2025-08-04 01:45:40 +0300

    register istegi ayarlandi

--- src/main/java/com/ekudu/eticaretkullanici/controller/AuthController.java
+++ src/main/java/com/ekudu/eticaretkullanici/controller/AuthController.java
@@ -46,7 +46,7 @@ public class AuthController {
 
 
         final Optional<UserEntity> userDetails = userService.getUserByUserName(loginRequest.getUsername());
-        UserEntity userEntity = userDetails.orElseThrow(() -> new RuntimeException("Kullanici bulunamadi."));
+        UserEntity userEntity = userDetails.orElseThrow(() -> new RuntimeException("Kullanıcı bulunamadı."));
 
 
         final String token = jwtUtil.generateToken(userEntity);
