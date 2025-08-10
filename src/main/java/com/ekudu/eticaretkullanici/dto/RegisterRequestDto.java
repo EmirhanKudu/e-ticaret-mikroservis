80625ea1550ef78bc4cd002d1361994892715195
Emirhan Kudu <ekudu16@gmail.com>
2025-08-10 23:37:44 +0300

    register: duzenleme

--- src/main/java/com/ekudu/eticaretkullanici/dto/RegisterRequestDto.java
+++ src/main/java/com/ekudu/eticaretkullanici/dto/RegisterRequestDto.java
@@ -2,7 +2,7 @@ package com.ekudu.eticaretkullanici.dto;
 
 import lombok.Data;
 @Data
-public class RegisterRequest {
+public class RegisterRequestDto {
     private String username;
     private Long phoneNumber;
     private String email;
