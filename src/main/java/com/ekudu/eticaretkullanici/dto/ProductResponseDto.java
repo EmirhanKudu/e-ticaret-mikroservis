907abe0ba3f7119c78782a47d62984cf167db6ce
Emirhan Kudu <ekudu16@gmail.com>
2025-08-12 15:18:24 +0300

    ProductService yazildi

---
+++ src/main/java/com/ekudu/eticaretkullanici/dto/ProductResponseDto.java
@@ -0,0 +1,16 @@
+package com.ekudu.eticaretkullanici.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductResponseDto {
+     private long id;
+     private String title;
+     private String description;
+     private double price;
+     private Integer stock;
+
+
+
+
+}
