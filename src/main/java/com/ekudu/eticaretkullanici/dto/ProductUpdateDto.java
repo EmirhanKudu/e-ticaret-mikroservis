bf1866fce04aab6d2b2d36fd8cae03bc9c7a85f5
Emirhan Kudu <ekudu16@gmail.com>
2025-08-12 15:18:30 +0300

    ProductService yazildi

---
+++ src/main/java/com/ekudu/eticaretkullanici/dto/ProductUpdateDto.java
@@ -0,0 +1,10 @@
+package com.ekudu.eticaretkullanici.dto;
+
+import java.math.BigDecimal;
+
+public class ProductUpdateDto {
+    private String title;
+    private double price;
+    private String description;
+
+}
