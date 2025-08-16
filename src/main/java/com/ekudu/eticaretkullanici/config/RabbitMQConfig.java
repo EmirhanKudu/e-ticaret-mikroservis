80073d6de9e57a63ceebacebf6121a85b8c40d22
Emirhan Kudu <ekudu16@gmail.com>
2025-08-16 19:40:34 +0300

    rabbitmq : duzenleme

--- src/main/java/com/ekudu/eticaretkullanici/config/RabbitMQConfig.java
+++ src/main/java/com/ekudu/eticaretkullanici/config/RabbitMQConfig.java
@@ -1,7 +1,10 @@
 package com.ekudu.eticaretkullanici.config;
+import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Queue;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +16,28 @@ public class RabbitMQConfig {
 
     public static final String USER_REG_QUEUE = "user.registration.queue";
 
+    public static final String ORDER_NOTIFICATION_QUEUE = "order.notification.queue";
+
+    @Bean
+    public Queue orderNotificationQueue() {
+        return new Queue(ORDER_NOTIFICATION_QUEUE, true);
+    }
 
     @Bean
     public Queue userRegistrationQueue() {
 
         return new Queue(USER_REG_QUEUE, true);
     }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+//                                         Jackson2JsonMessageConverter converter) {
+//        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//        template.setMessageConverter(converter);
+//    }
 }
