53228c2a68e6a4af2c335fb7530df6fe296c497d
Emirhan Kudu <ekudu16@gmail.com>
2025-08-16 19:41:58 +0300

    productrepository: duzenleme

--- src/main/java/com/ekudu/eticaretkullanici/repository/ProductRepository.java
+++ src/main/java/com/ekudu/eticaretkullanici/repository/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.ekudu.eticaretkullanici.repository;
 
 import com.ekudu.eticaretkullanici.model.ProductEntity;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     Optional<ProductEntity> findByTitle(String title);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from ProductEntity p where p.id = :id")
+    Optional<ProductEntity> findByIdForUpdate(@Param("id") Long id);
 }
