20b10cc7bf341465bb690b883dd425945257d66c
Emirhan Kudu <ekudu16@gmail.com>
2025-08-18 16:25:12 +0300

    Cart sistemi : revize

---
+++ src/main/java/com/ekudu/eticaretkullanici/dto/CartProductDto.java
@@ -0,0 +1,13 @@
+package com.ekudu.eticaretkullanici.dto;
+
+import lombok.Data;
+
+@Data
+public class CartProductDto {
+
+    private int quantity;
+    private String title;
+    private double price;
+
+
+}
