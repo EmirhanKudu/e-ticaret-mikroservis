907c77edff5ffc83e779d8e3d9a4baed0bb36ea2
Emirhan Kudu <ekudu16@gmail.com>
2025-08-18 16:25:33 +0300

    Cart service : duzenleme

--- src/main/java/com/ekudu/eticaretkullanici/service/impl/CartServiceImpl.java
+++ src/main/java/com/ekudu/eticaretkullanici/service/impl/CartServiceImpl.java
@@ -1,16 +1,19 @@
 package com.ekudu.eticaretkullanici.service.impl;
 
 import com.ekudu.eticaretkullanici.cache.CartCacheService;
-import com.ekudu.eticaretkullanici.dto.AddCartRequestDto;
-import com.ekudu.eticaretkullanici.dto.AddCartResponseDto;
+import com.ekudu.eticaretkullanici.dto.*;
 import com.ekudu.eticaretkullanici.model.UserEntity;
 import com.ekudu.eticaretkullanici.service.CartService;
+import com.ekudu.eticaretkullanici.service.ProductService;
 import com.ekudu.eticaretkullanici.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -18,6 +21,7 @@ import java.util.Optional;
 public class CartServiceImpl implements CartService {
     final CartCacheService cartCacheService;
     final UserService userService;
+    final ProductService productService;
 
 
     @Override
@@ -34,6 +38,49 @@ public class CartServiceImpl implements CartService {
         addCartResponseDto.setSuccess(true);
         addCartResponseDto.setMessage("Ürün sepete başarıyla eklenmiştir!");
         return addCartResponseDto;
+    }
+
+    @Override
+    public CartAllProductDto getAllCart() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        List<CartProductDto> cartProductDtoList = new ArrayList<>();
+        Optional<UserEntity> user = userService.getUserByUserName(name);
+        if(user.isEmpty()){
+            throw new RuntimeException("Kullanıcı bulunamadı");
+        }
+        Long userId = user.get().getId();
+        Map<Object,Object> getCartMap = cartCacheService.getCart(userId);
+        double totalPrice = 0;
+        for(Object productId : getCartMap.keySet()) {
+            String quantity = getCartMap.get(productId).toString();
+            int quantityInt = Integer.parseInt(quantity);
+            ProductResponseDto productResponseDto = productService.getProductById(Long.parseLong(productId.toString()));
+            totalPrice += productResponseDto.getPrice() * quantityInt;
+            CartProductDto cartProductDto = new CartProductDto();
+            cartProductDto.setQuantity(quantityInt);
+            cartProductDto.setPrice(productResponseDto.getPrice());
+            cartProductDto.setTitle(productResponseDto.getTitle());
+            cartProductDtoList.add(cartProductDto);
+        }
+        CartAllProductDto cartAllProductDto = new CartAllProductDto();
+        cartAllProductDto.setTotalPrice(totalPrice);
+        cartAllProductDto.setCartProductDtoList(cartProductDtoList);
+
+        return cartAllProductDto;
+    }
+
+    @Override
+    public String clearAllCart() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        Optional<UserEntity> user = userService.getUserByUserName(name);
+        if(user.isEmpty()){
+            throw new RuntimeException("Kullanıcı bulunamadı");
+        }
+        Long userId = user.get().getId();
+        cartCacheService.clearCart(userId);
+        return "Sepet boşaltıldı";
     }
 
 
