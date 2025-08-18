dece1f1188fa6a69dd19bd6c342e57496d075127
Emirhan Kudu <ekudu16@gmail.com>
2025-08-18 16:25:37 +0300

    Cart service : duzenleme

--- src/main/java/com/ekudu/eticaretkullanici/service/CartService.java
+++ src/main/java/com/ekudu/eticaretkullanici/service/CartService.java
@@ -2,9 +2,17 @@ package com.ekudu.eticaretkullanici.service;
 
 import com.ekudu.eticaretkullanici.dto.AddCartRequestDto;
 import com.ekudu.eticaretkullanici.dto.AddCartResponseDto;
+import com.ekudu.eticaretkullanici.dto.CartAllProductDto;
 
 public interface CartService {
 
     AddCartResponseDto addCart (AddCartRequestDto addCartRequestDto);
 
+
+
+    CartAllProductDto getAllCart();
+
+
+
+    String clearAllCart();
 }
