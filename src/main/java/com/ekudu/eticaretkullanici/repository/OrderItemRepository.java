2f6908e215a6d4531e5d8c17f681a3974bd48097
Emirhan Kudu <ekudu16@gmail.com>
2025-08-19 16:25:37 +0300

    OrderItemRepository : guncellendi

--- src/main/java/com/ekudu/eticaretkullanici/repository/OrderItemRepository.java
+++ src/main/java/com/ekudu/eticaretkullanici/repository/OrderItemRepository.java
@@ -1,8 +1,12 @@
 package com.ekudu.eticaretkullanici.repository;
 
+import com.ekudu.eticaretkullanici.model.OrderEntity;
 import com.ekudu.eticaretkullanici.model.OrderItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItemEntity,Long> {
+    public List<OrderItemEntity> findByOrderEntity(OrderEntity orderEntity);
 
 }
