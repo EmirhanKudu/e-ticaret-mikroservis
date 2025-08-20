edd2fe030533a67383dfafd181e6babb8fe20a2e
Emirhan Kudu <ekudu16@gmail.com>
2025-08-20 21:48:04 +0300

    RefreshToken : revize

--- src/main/java/com/ekudu/eticaretkullanici/cache/RefreshingService.java
+++ src/main/java/com/ekudu/eticaretkullanici/cache/RefreshingService.java
@@ -5,11 +5,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.UUID;
+
 @Service
 
 public class RefreshingService {
 
-
+    String key = "refresh_token:";
 
     private final RedisTemplate<String, String> redis;
     public  RefreshingService(RedisTemplate<String, String> redis) {
@@ -17,8 +20,27 @@ public class RefreshingService {
         this.redis = redis;
     }
 
-//    public void addRefreshToken(Long userId, String refreshToken) {
-//         String usId = userId.toString();
-//        redis.opsForHash().put(usId,refreshToken);
-//    }
+    public void addRefreshToken(Long id, String refreshToken) {
+
+        redis.opsForHash().put(key,refreshToken,id);
+    }
+
+    public Long getIdByRefreshToken(String refreshToken) {
+        Long usId = Long.parseLong(redis.opsForHash().get(key,refreshToken).toString());
+        return usId;
+    }
+
+    public String getRefreshTokenById(Long id) {
+        String refreshToken = null;
+        Map<Object, Object> refreshMap = redis.opsForHash().entries(key);
+        for (Map.Entry<Object, Object> entry : refreshMap.entrySet()) {
+            if (entry.getValue().equals(id)) {
+                refreshToken = entry.getKey().toString();
+            }
+        }
+        return refreshToken;
+
+
+    }
+
 }
